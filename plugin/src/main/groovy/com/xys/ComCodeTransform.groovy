0a7ccd629637cb952b9b9b9424812e89ec2a211b
Fox.hu <Hujia0523@gmail.com>
2018-10-10 17:41:37 +0800

    add transform
    add transform

---
+++ plugin/src/main/groovy/com/xys/ComCodeTransform.groovy
@@ -0,0 +1,127 @@
+package com.xys
+
+import com.android.build.api.transform.*
+import com.android.build.gradle.internal.pipeline.TransformManager
+import javassist.ClassPool
+import javassist.CtClass
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.commons.io.FileUtils
+import org.gradle.api.Project
+
+//https://blog.csdn.net/yulong0809/article/details/77752098
+class ComCodeTransform extends Transform {
+    private Project project
+    ClassPool classPool
+    String applicationName
+
+    ComCodeTransform(Project project) {
+        this.project = project
+    }
+
+    @Override
+    void transform(TransformInvocation transformInvocation) throws TransformException, InterruptedException, IOException {
+        getRealApplicationName()
+        classPool = new ClassPool()
+        project.android.bootClasspath.each { classPool.appendClassPath((String) it.absolutePath) }
+        //获取app和applike类集合
+        def box = ConvertUtils.toCtClass(transformInvocation.inputs, classPool)
+
+        List<CtClass> applications = new ArrayList<>()
+        List<CtClass> activators = new ArrayList<>()
+
+        for (CtClass ctClass : box) {
+            if (isApplication(ctClass)) {
+                applications.add(ctClass)
+                continue
+            }
+            if (isActivator(ctClass)) {
+                activators.add(ctClass)
+            }
+        }
+
+        for (CtClass ctClass : applications) {
+            System.out.println("application is   " + ctClass.getName())
+        }
+        for (CtClass ctClass : activators) {
+            System.out.println("applicationlike is   " + ctClass.getName())
+        }
+
+        transformInvocation.inputs.each {
+            TransformInput input ->
+                input.jarInputs.each {
+                    JarInput jarInput ->
+                        def jarName = jarInput.name
+                        def md5Name = DigestUtils.md5Hex(jarInput.file.getAbsolutePath())
+                        if (jarName.endsWith(".jar")) {
+                            jarName = jarName.substring(0, jarName.length() - 4)
+                        }
+                        def location = transformInvocation.outputProvider.getContentLocation(jarName + md5Name, jarInput.contentTypes, jarInput.scopes, Format.JAR)
+                        FileUtils.copyFile(jarInput.file, location)
+                }
+        }
+    }
+
+    void getRealApplicationName() {
+        applicationName = project.extensions.combuild.applicationName
+        if (applicationName == null || applicationName.isEmpty()) {
+            throw new RuntimeException("you should set applicationName in combuild")
+        }
+
+    }
+
+    private boolean isApplication(CtClass ctClass) {
+        try {
+            if (applicationName != null && applicationName.equals(ctClass.getName())) {
+                return true
+            }
+        } catch (Exception e) {
+            println "class not found exception class name:  " + ctClass.getName()
+        }
+        return false
+    }
+
+    private boolean isActivator(CtClass ctClass) {
+        try {
+            for (CtClass ctClassInter : ctClass.getInterfaces()) {
+                if ("com.component.componentlib.applicationlike.IApplicationLike".equals(ctClassInter.name)) {
+                    return true
+                }
+            }
+        } catch (Exception e) {
+            println "class not found exception class name:  " + ctClass.getName()
+        }
+        return false
+    }
+
+    @Override
+    String getName() {
+        return "ComponentCode"
+    }
+
+    //  需要处理的数据类型，有两种枚举类型
+    //  CLASSES和RESOURCES，CLASSES代表处理的java的class文件，RESOURCES代表要处理java的资源
+    @Override
+    Set<QualifiedContent.ContentType> getInputTypes() {
+        return TransformManager.CONTENT_CLASS
+    }
+
+    //    指Transform要操作内容的范围，官方文档Scope有7种类型：
+    //
+    //    EXTERNAL_LIBRARIES        只有外部库
+    //    PROJECT                       只有项目内容
+    //    PROJECT_LOCAL_DEPS            只有项目的本地依赖(本地jar)
+    //    PROVIDED_ONLY                 只提供本地或远程依赖项
+    //    SUB_PROJECTS              只有子项目。
+    //    SUB_PROJECTS_LOCAL_DEPS   只有子项目的本地依赖项(本地jar)。
+    //    TESTED_CODE                   由当前变量(包括依赖项)测试的代码
+    @Override
+    Set<? super QualifiedContent.Scope> getScopes() {
+        return TransformManager.SCOPE_FULL_PROJECT
+    }
+
+    //指明当前Transform是否支持增量编译
+    @Override
+    boolean isIncremental() {
+        return false
+    }
+}
